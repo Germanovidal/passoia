5c281fe97b01ec67ed0b5443761067dbdd1008da
Germano <ggermano.vidal@gmail.com>
2025-11-12 10:36:24 -0300

    Criando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+// Arquivo principal
+// Para escrita do nome do componente funcional usamos sempre a primeira letra maiuscula, PascalCase
+function App() {
+  return(
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
