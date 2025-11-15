e94f516e8426acd0a7241ea064bac295fb517680
Germano <ggermano.vidal@gmail.com>
2025-11-15 12:31:14 -0300

    Adiciona estilo aos cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Lancamentos from "./components/Lancamentos"
 import Hero from "./components/Hero"
+import Makeup from "./components/Makeup"
 // Arquivo principal
 // Para escrita do nome do componente funcional usamos sempre a primeira letra maiuscula, PascalCase
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header/>
       <Hero/>
+      <Makeup/>
       <Lancamentos/>
     </>
   )
---
+++ src/components/Makeup/index.jsx
@@ -0,0 +1,17 @@
+import "./makup.scss"
+
+function Makeup() {
+    return (
+        <section>
+            <h2>LOOKS E DICAS DE MAQUIAGEM</h2>
+            <div className="cards">
+                <article className="card-labios"></article>
+                <article className="card-olhos"></article>
+                <article className="card-rosto"></article>
+                <article className="card-tendencia"></article>
+            </div>
+        </section>
+    )
+}
+
+export default Makeup
