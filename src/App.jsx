ac53d180745f79cea5298ed0562e9e099bbafcce
Germano <ggermano.vidal@gmail.com>
2025-11-15 00:18:54 -0300

    Cria sessao do banner principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header"
 import Lancamentos from "./components/Lancamentos"
+import Hero from "./components/Hero"
 // Arquivo principal
 // Para escrita do nome do componente funcional usamos sempre a primeira letra maiuscula, PascalCase
 function App() {
   return(
     <>
       <Header/>
+      <Hero/>
       <Lancamentos/>
     </>
   )
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,11 @@
+import "./hero.scss"
+
+function Hero() {
+    return (
+        <section className="hero">
+            <div className="hero-img"></div>
+        </section>
+    )
+}
+
+export default Hero
